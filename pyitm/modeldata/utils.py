dbe71e225e2ec27ec0d91404eb686fa54562b5bb
Aaron Ridley <ridley@umich.edu>
2025-05-29 14:56:14 -0400

    FEAT: slice data and calc edges

---
+++ pyitm/modeldata/utils.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+# ----------------------------------------------------------------------------
+# take a dictionary containing all of the model data and
+# return slices. The data should have the shape:
+# [nTimes, nVars, nLons, nLats, nAlts]
+# or
+# [nTimes, nLons, nLats, nAlts]
+# ----------------------------------------------------------------------------
+
+def data_slice(allData3D, iLon = -1, iLat = -1, iAlt = -1):
+
+    nTimes = allData3D['nTimes']
+    nVars = allData3D['nVars']
+    nLons = allData3D['nLons']
+    nLats = allData3D['nLats']
+    nAlts = allData3D['nAlts']
+
+    if (nVars > 1):
+        if (iAlt > -1):
+            slices = np.zeros((nTimes, nVars, nLons, nLats))
+            slices[:, :, :, :] = allData3D['data'][:, :, :, :, iAlt]
+        elif (iLat > -1):
+            slices = np.zeros((nTimes, nVars, nLons, nAlts))
+            slices[:, :, :, :] = allData3D['data'][:, :, :, iLat, :]
+        else:
+            slices = np.zeros((nTimes, nVars, nLats, nAlts))
+            slices[:, :, :, :] = allData3D['data'][:, :, iLon, :, :]
+    else:
+        if (iAlt > -1):
+            slices = np.zeros((nTimes, nLons, nLats))
+            slices[:, :, :] = allData3D['data'][:, :, :, iAlt]
+        elif (iLat > -1):
+            slices = np.zeros((nTimes, nLons, nAlts))
+            slices[:, :, :] = allData3D['data'][:, :, iLat, :]
+        else:
+            slices = np.zeros((nTimes, nLats, nAlts))
+            slices[:, :, :] = allData3D['data'][:, iLon, :, :]
+
+    return slices
+
+# ----------------------------------------------------------------------------
+# This function calculates the edges of cells based on the centers of the cells
+# it assumes a 1D array.
+# ----------------------------------------------------------------------------
+
+def move_centers_to_edges(pos):
+    edges = (pos[1:] + pos[:-1])/2
+    dpLeft = pos[1] - pos[0]
+    dpRight = pos[-1] - pos[-2]
+    edges = np.append(edges[0] - dpLeft, edges)
+    edges = np.append(edges, dpRight + edges[-1])
+    return edges
+
+
