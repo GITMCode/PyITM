139b0521d64bc987d5379eeea552e157c8674e34
Aaron Bukowski <abukowski21@gmail.com>
2025-09-18 12:00:56 -0400

    plotsat- more consistent variable names...

--- bin/plot_satellite.py
+++ bin/plot_satellite.py
@@ -51,38 +51,38 @@ def makesatplot(satDataDict, savepath, verbose=False, saveName=None):
 
     fig, axs = plt.subplots(2, 1, figsize=(10, 10), sharex=True)
 
-    didSmooth = 'smoothed_sat_density' in satDataDict.keys() 
+    didSmooth = 'smoothed_sat_rho' in satDataDict.keys() 
     
     if didSmooth:
         axs[0].plot(satDataDict['times'], satDataDict['model_rho'], 'b', 
                     alpha = 0.1)
-        axs[0].plot(satDataDict['times'], satDataDict['sat_density'], 'r', 
+        axs[0].plot(satDataDict['times'], satDataDict['sat_rho'], 'r', 
                     alpha = 0.1)
         axs[0].plot(satDataDict['times'], satDataDict['smoothed_model_rho'], 'b', 
                     label="Modeled Density")
-        axs[0].plot(satDataDict['times'], satDataDict['smoothed_sat_density'], 'r', 
+        axs[0].plot(satDataDict['times'], satDataDict['smoothed_sat_rho'], 'r', 
                     label=satDataDict['sat_name'].upper() + " Density")
     else:
         axs[0].plot(satDataDict['times'], satDataDict['model_rho'], 'b', 
                     label="Modeled Density")
-        axs[0].plot(satDataDict['times'], satDataDict['sat_density'], 'r', 
+        axs[0].plot(satDataDict['times'], satDataDict['sat_rho'], 'r', 
                     label=satDataDict['sat_name'].upper() + " Density")
     axs[0].legend()
 
     
     if didSmooth:
         axs[1].plot(satDataDict['times'], 
-                    100*(satDataDict['sat_density']- satDataDict['model_rho'])
-                    /satDataDict['sat_density'],
+                    100*(satDataDict['sat_rho']- satDataDict['model_rho'])
+                    /satDataDict['sat_rho'],
                     alpha = 0.1, color='k')
         axs[1].plot(satDataDict['times'], 
-                    100*(satDataDict['smoothed_sat_density']-satDataDict['smoothed_model_rho'])
-                    /satDataDict['smoothed_sat_density'],
+                    100*(satDataDict['smoothed_sat_rho']-satDataDict['smoothed_model_rho'])
+                    /satDataDict['smoothed_sat_rho'],
                     color='k')
     else:
         axs[1].plot(satDataDict['times'], 
-                    100*(satDataDict['sat_density']- satDataDict['model_rho'])
-                    /satDataDict['sat_density'],
+                    100*(satDataDict['sat_rho']- satDataDict['model_rho'])
+                    /satDataDict['sat_rho'],
                     color='k')
     
     axs[1].hlines(0, min(satDataDict['times']), max(satDataDict['times']),
@@ -90,7 +90,7 @@ def makesatplot(satDataDict, savepath, verbose=False, saveName=None):
 
     axs[1].set_ylabel(' Density (kg/m3)')
     axs[1].set_ylabel(' Diff (%)')
-    fig.suptitle(satDataDict['sat_name'])
+    fig.suptitle(satDataDict['sat_name'].upper())
 
     i = 0
     outdate = satDataDict['times'][0].strftime('%Y%m%d')
@@ -107,7 +107,7 @@ def makesatplot(satDataDict, savepath, verbose=False, saveName=None):
     print('--> Saving plot as: ' + savename)
     plt.savefig(savename)
 
-    return    
+    return
 
 def write_out_data(satDataDict, savepath, verbose=False):
     """
