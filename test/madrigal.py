a08041b466c1df3383c59f7b9ec075ee010ca82e
Aaron Bukowski <abukowski21@gmail.com>
2025-09-30 14:01:43 -0400

    TEST: tests for madrigal readers

---
+++ test/madrigal.py
@@ -0,0 +1,15 @@
+from pyitm.fileio.madrigalio import read_madrigal_file
+
+data = read_madrigal_file('data/madrigal/gps021221g.002.nc', verbose=True)
+for k in data.keys():
+    print(k, data[k].shape)
+
+data = read_madrigal_file('data/madrigal/gps021221g.002.hdf5', verbose=True)
+for k in data.keys():
+    print(k, data[k].shape)
+
+
+data = read_madrigal_file('data/madrigal/dms_20150716_17e.001.hdf5', verbose=True)
+for k in data.keys():
+    print(k, data[k].shape)
+print(data['times'][0:10])
