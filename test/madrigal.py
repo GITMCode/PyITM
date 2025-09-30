5a0a898d388eb41cb4c084897eb40fb8d8e107e6
Aaron Bukowski <abukowski21@gmail.com>
2025-09-30 14:49:56 -0400

    change files referenced in madrigal tests. they were too large to push

--- test/madrigal.py
+++ test/madrigal.py
@@ -4,12 +4,13 @@ data = read_madrigal_file('data/madrigal/gps021221g.002.nc', verbose=True)
 for k in data.keys():
     print(k, data[k].shape)
 
-data = read_madrigal_file('data/madrigal/gps021221g.002.hdf5', verbose=True)
+
+data = read_madrigal_file('data/madrigal/dms_20150716_18s1.001.hdf5', verbose=True)
 for k in data.keys():
     print(k, data[k].shape)
+print(data['times'][0:10])
 
-
-data = read_madrigal_file('data/madrigal/dms_20150716_17e.001.hdf5', verbose=True)
+data = read_madrigal_file('data/cul130621.17201.hdf5', verbose=True)
 for k in data.keys():
     print(k, data[k].shape)
 print(data['times'][0:10])
