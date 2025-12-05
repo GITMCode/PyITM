24d09f4974ca900ec25b33f61ba7d74339b826af
Aaron Ridley <ridley@umich.edu>
2025-12-05 15:42:10 -0500

    add functionality

--- pyitm/plotting/line_plots.py
+++ pyitm/plotting/line_plots.py
@@ -13,7 +13,11 @@ from pyitm.plotting import axes
 #
 # ----------------------------------------------------------------------------
 
-def lineplot_data(data, outFile=None, fig=None, ax=None, vars = None):
+def lineplot_data(data, outFile=None, fig=None, ax=None, vars = None,
+                  linewidth = 1.0, color = None, linestyle = None,
+                  xStart = None, xEnd = None,
+                  yMin = None, yMax = None,
+                  xlabel = None, label = None, nLabels = None, title = None):
     """
     Make lineplot out of data
 
@@ -39,19 +43,39 @@ def lineplot_data(data, outFile=None, fig=None, ax=None, vars = None):
         yTop = 0.05
         yBuf = 0.05
         ax = axes.get_axes_one_column(fig,
-                                    nVars,
-                                    yBot,
-                                    yTop,
-                                    yBuf)
+                                      nVars,
+                                      yBot,
+                                      yTop,
+                                      yBuf)
 
     times = data['times']
+    if (xStart == None):
+        xStart = times[0]
+    if (xEnd == None):
+        xEnd = times[-1]
     for iPlot, var in enumerate(vars):
-        ax[iPlot].plot(times, data[var])
-        ax[iPlot].set_ylabel(var)
-    
-    sTime = times[0].strftime("%d %b %Y %H:%M:%S UT")
-    eTime = times[-1].strftime("%d %b %Y %H:%M:%S UT")
+        ax[iPlot].plot(times, data[var], \
+                       linewidth = linewidth, \
+                       linestyle = linestyle, \
+                       label = label, \
+                       color = color)
+        if (xlabel == None):
+            xlabel = var
+        ax[iPlot].set_ylabel(xlabel)
+        ax[iPlot].set_xlim(xStart, xEnd)
+        if (title != None):
+            ax[iPlot].set_title(title)
+        if ((yMin != None) and (yMax != None)):
+            ax[iPlot].set_ylim(yMin, yMax)
+
+    sTime = xStart.strftime("%d %b %Y %H:%M:%S UT")
+    eTime = xEnd.strftime("%d %b %Y %H:%M:%S UT")
     ax[-1].set_xlabel('Time from ' + sTime + ' to ' + eTime)
+    if (label != None):
+        if (nLabels != None):
+            ax[-1].legend(ncol = int(np.ceil(nLabels/5)))
+        else:
+            ax[-1].legend()
 
     if ('title' in data.keys()):
         ax[0].set_title(data['title'])
