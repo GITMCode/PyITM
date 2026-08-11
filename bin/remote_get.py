2e774f2a6e26233efe1e2496cb3096f808e428a3
Aaron Ridley <ridley@umich.edu>
2026-08-11 11:19:38 -0400

    allow deletion of files after get

--- bin/remote_get.py
+++ bin/remote_get.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 
 import argparse
-import os
-import re
 
-IsVerbose = False
+from pyitm.fileio import remote
 
 # ----------------------------------------------------------------------
 # Function to parse input arguments
@@ -36,95 +34,18 @@ def parse_args_post():
                         help = 'remote directory to use (default none)',
                         default = 'none')
     
-    parser.add_argument('-v', '--verbose',
+    parser.add_argument('-v',
                         help = 'Run with verbose',
+                        action = 'store_true')
+
+    parser.add_argument('-rm',
+                        help = 'remove remote files after getting',
                         action = 'store_true')
     
     args = parser.parse_args()
 
     return args
 
-# ----------------------------------------------------------------------
-# parse remote file
-#   - remote file has the form:
-# username
-# remote server
-# remote directory
-# ----------------------------------------------------------------------
-
-def parse_remote_file(file):
-
-    if (IsVerbose):
-        print('Reading file ', file)
-    
-    fpin = open(file, 'r')
-    user = fpin.readline()
-    server = fpin.readline()
-    dir = fpin.readline()
-    fpin.close()
-
-    remote = {'user': user.strip(),
-              'server': server.strip(),
-              'dir': dir.strip()}
-    return remote
-
-# ----------------------------------------------------------------------
-# do system command
-# ----------------------------------------------------------------------
-
-def run_command(command):
-    if (IsVerbose):
-        print("   -> Running Command : ")
-        print("      ", command)
-    os.system(command)
-    return True
-
-# ----------------------------------------------------------------------
-# Check inputs:
-# ----------------------------------------------------------------------
-
-def check_inputs(user, server, dir):
-    
-    IsRemote = True
-    if ((len(user) == 0) or (user == 'none')):
-        if (IsVerbose):
-            print("Can't parse user information")
-        IsRemote = False
-    if ((len(server) == 0) or (server == 'none')):
-        if (IsVerbose):
-            print("Can't parse server information")
-        IsRemote = False
-    if ((len(dir) == 0) or (dir == 'none')):
-        if (IsVerbose):
-            print("Can't parse dir information")
-        IsRemote = False
-
-    return IsRemote
-
-# ----------------------------------------------------------------------
-# load remote file
-# ----------------------------------------------------------------------
-
-def load_remote_file(args):
-
-    remoteFile = args.remotefile
-    
-    # figure out remote system information:
-    if (os.path.exists(remoteFile)):
-        print('Found file: ', remoteFile)
-        remote = parse_remote_file(remoteFile)
-        user = remote['user']
-        server = remote['server']
-        dir = remote['dir']
-    else:
-        user = args.user
-        server = args.server
-        dir = args.dir
-
-    # Check remote system inputs:
-    IsRemote = check_inputs(user, server, dir)
-    return IsRemote, user, server, dir
-    
 # ----------------------------------------------------------------------
 # Main Code
 # ----------------------------------------------------------------------
@@ -135,14 +56,21 @@ if __name__ == '__main__':  # main code block
 
     # make local variables for arguments:
     files = args.files
-    IsVerbose = args.verbose
+    IsVerbose = args.v
+    doRemove = args.rm
 
-    IsRemote, user, server, dir = load_remote_file(args)
-    remote = user + '@' + server + ':' + dir + '/'
+    IsRemote, user, server, dir = \
+        remote.load_remote_file(args.remotefile, IsVerbose = IsVerbose)
 
-    m = re.search('(.*)(star)(.*)', files)
-    if m:
-        files = '"' + m.group(1) + '*' + m.group(3) + '"'
-    remote = remote + files
-    command = 'rsync -v ' + remote + ' .'
-    run_command(command)
+    DoRemove = False
+    didWork = remote.pull_files(files, user, server, dir, doRemove, \
+                                IsVerbose = IsVerbose)
+    
+    #remote = user + '@' + server + ':' + dir + '/'
+    #
+    #m = re.search('(.*)(star)(.*)', files)
+    #if m:
+    #    files = '"' + m.group(1) + '*' + m.group(3) + '"'
+    #remote = remote + files
+    #command = 'rsync -v ' + remote + ' .'
+    #system.run_command(command)
